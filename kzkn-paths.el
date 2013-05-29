391970d6d87e2d6f05df89d69c43fa9ccddfd3f2
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2013-05-29 11:08:34 +0900

    refactoring: extract function "vendor-modules"

--- kzkn-paths.el
+++ kzkn-paths.el
@@ -3,10 +3,13 @@
 (add-to-list 'load-path user-emacs-directory)
 (add-to-list 'load-path vendor-dir)
 
+(defun vendor-modules ()
+  (remove-if (lambda (file)
+               (or (string-match-p "\\(?:\\.\\|\\.\\.\\)$" file)
+                   (not (file-directory-p file))))
+             (directory-files vendor-dir t)))
+
 (mapc (lambda (dir) (add-to-list 'load-path dir))
-      (remove-if (lambda (file)
-                   (or (string-match-p "\\(?:\\.\\|\\.\\.\\)$" file)
-                       (not (file-directory-p file))))
-                 (directory-files vendor-dir t)))
+      (vendor-modules))
 
 (provide 'kzkn-paths)
