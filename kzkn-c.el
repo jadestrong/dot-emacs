04350b377e97c4e63af9a4acdb3900f0121176f1
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2013-07-08 14:29:05 +0900

    フックを関数化

--- kzkn-c.el
+++ kzkn-c.el
@@ -1,9 +1,10 @@
 ;;; kzkn-c.el --- C programming stuff
 
-(add-hook 'c-mode-hook
-          (lambda ()
-            (c-set-style "K&R")
-            (setq tab-width 2)
-            (setq c-basic-offset 2)))
+(defun kzkn-c-mode-hook ()
+  (c-set-style "K&R")
+  (setq tab-width 2)
+  (setq c-basic-offset 2))
+
+(add-hook 'c-mode-hook 'kzkn-c-mode-hook)
 
 (provide 'kzkn-c)
