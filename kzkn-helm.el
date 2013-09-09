5be780568bcc7405ca050f27f675ead32637f8c4
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2013-09-09 18:18:47 +0900

    helm-gtags-parse-file のキーボードショートカットを追加

--- kzkn-helm.el
+++ kzkn-helm.el
@@ -13,14 +13,16 @@
 (defalias 'ag-file 'helm-ag-this-file)
 
 (require 'helm-gtags)
-(add-hook 'c-mode-hook 'helm-gtags-mode)
-(add-hook 'c++-mode-hook 'helm-gtags-mode)
+(add-hook 'c-mode-common-hook 'helm-gtags-mode)
 (add-hook 'helm-gtags-mode-hook
           (lambda ()
             (local-set-key (kbd "M-.") 'helm-gtags-find-tag)
             (local-set-key (kbd "M-,") 'helm-gtags-find-rtag)
             (local-set-key (kbd "M-s") 'helm-gtags-find-symbol)
             (local-set-key (kbd "M-*") 'helm-gtags-pop-stack)
-            (local-set-key (kbd "C-c C-f") 'helm-gtags-find-files)))
+            (local-set-key (kbd "C-c C-f") 'helm-gtags-find-files)
+            (local-set-key (kbd "C-c o") 'helm-gtags-parse-file)))
+
+
 
 (provide 'kzkn-helm)
