a6078cd1e455ec051cef430e6dfb4086c4565abd
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2017-10-17 22:17:51 +0900

    使ってない関数を削除

--- defuns.el
+++ defuns.el
@@ -1,50 +1,6 @@
 (eval-when-compile
   (require 'cl))
 
-(defun google ()
-  (interactive)
-  (browse-url
-   (concat
-    "http://www.google.com/search?ie=utf-8&oe=utf-8&q="
-    (if (region-active-p)
-        (buffer-substring (region-beginning) (region-end))
-      (read-string "Query: ")))))
-
-(defun buffer-current-line-string ()
-  (save-excursion
-    (buffer-substring (progn (beginning-of-line) (point))
-                      (progn (end-of-line) (point)))))
-
-(defun buffer-current-line-starts-with (prefix)
-  (let ((line (buffer-current-line-string)))
-    (and (<= (length prefix) (length line))
-         (equal prefix (substring line 0 (length prefix))))))
-
-(defun open-path ()
-  (interactive)
-  (save-excursion
-    (let ((path (if (%thunar-path-p)
-                    (buffer-current-line-string)
-                  (%win-to-thunar))))
-      (start-process "open-path" nil "/usr/bin/thunar" path))))
-
-(defun %thunar-path-p ()
-  (buffer-current-line-starts-with "smb:"))
-
-(defun %win-to-thunar ()
-  (let ((orig-path (buffer-current-line-string)))
-    (with-temp-buffer
-      (insert orig-path)
-      (%replace-path-separators)
-      (buffer-string))))
-
-(defun %replace-path-separators ()
-  (let ((end (progn (end-of-line) (point))))
-    (beginning-of-line)
-    (insert "smb:")
-    (while (and (search-forward "\\" nil t) (<= (point) end))
-      (replace-match "/"))))
-
 (defun resize-window ()
   "Resize the current window."
   (interactive)
